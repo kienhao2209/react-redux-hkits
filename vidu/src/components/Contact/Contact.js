b74e1f71992c93f5a67049884a0d3ffac35ecaf7
Huynh Kien Hao <kienhao2209@gmail.com>
2024-09-19 23:19:41 +0700

    video 46

--- vidu/src/components/Contact/Contact.js
+++ vidu/src/components/Contact/Contact.js
@@ -1,122 +1,86 @@
 import React, { Component } from 'react';
+import { Navigate } from 'react-router-dom';
 
 class Contact extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isNavigate: false
+        }
+    }
+
+    submitForm = (event) => {
+        event.preventDefault();
+        this.setState({
+            isNavigate: true
+        });
+    }
+
     render() {
+        if (this.state.isNavigate) {
+            return <Navigate to="/home" />
+        }
         return (
             <>
-            <header className="masthead tintuc">
-                <div className="container px-5">
-                    <div className="row gx-5 align-items-center">
-                        <div className="col-lg-12">
-                        {/* Mashead text and app badges*/}
-                            <div className="mb-5 mb-lg-0 text-lg-start">
-                                <h1 className="display-1 lh-1 mb-3 text-center">Trang liên hệ</h1>
+                <header className="masthead tintuc">
+                    <div className="container px-5">
+                        <div className="row gx-5 align-items-center">
+                            <div className="col-lg-12">
+                                {/* Mashead text and app badges*/}
+                                <div className="mb-5 mb-lg-0 text-lg-start">
+                                    <h1 className="display-1 lh-1 mb-3 text-center">Trang liên hệ</h1>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </header>
-            {/* Begin Contact */}
-            <section className="page-section" id="contact">
-                <div className="container">
-                    {/* Contact Section Heading*/}
-                    <h2 className="page-section-heading text-center text-uppercase text-secondary mb-3">
-                        Liên hệ với chúng tôi
-                    </h2>
-                    {/* Contact Section Form*/}
-                    <div className="row justify-content-center">
-                        <div className="col-lg-8 col-xl-7">
-                            {/* * * * * * * * * * * * * * * **/}
-                            {/* * * SB Forms Contact Form * **/}
-                            {/* * * * * * * * * * * * * * * **/}
-                            {/* This form is pre-integrated with SB Forms.*/}
-                            {/* To make this form functional, sign up at*/}
-                            {/* https://startbootstrap.com/solution/contact-forms*/}
-                            {/* to get an API token!*/}
-                            <form id="contactForm" data-sb-form-api-token="API_TOKEN">
-                                {/* Name input*/}
-                                <div className="form-floating mb-3">
-                                    <input
-                                        className="form-control is-invalid"
-                                        id="name"
-                                        type="text"
-                                        placeholder="Enter your name..."
-                                        data-sb-validations="required"
-                                        data-sb-can-submit="no"
-                                    />
-                                    <label htmlFor="name">Full name</label>
-                                    <div className="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
-                                </div>
-                                {/* Email address input*/}
-                                <div className="form-floating mb-3">
-                                    <input
-                                        className="form-control"
-                                        id="email"
-                                        type="email"
-                                        placeholder="name@example.com"
-                                        data-sb-validations="required,email"
-                                        data-sb-can-submit="no"
-                                    />
-                                    <label htmlFor="email">Email address</label>
-                                    <div className="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
-                                    <div className="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
-                                </div>
-                                {/* Phone number input*/}
-                                <div className="form-floating mb-3">
-                                    <input
-                                        className="form-control"
-                                        id="phone"
-                                        type="tel"
-                                        placeholder="(123) 456-7890"
-                                        data-sb-validations="required"
-                                        data-sb-can-submit="no"
-                                    />
-                                    <label htmlFor="phone">Phone number</label>
-                                    <div className="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
-                                </div>
-                                {/* Message input*/}
-                                <div className="form-floating mb-3">
-                                    <textarea
-                                        className="form-control"
-                                        id="message"
-                                        type="text"
-                                        placeholder="Enter your message here..."
-                                        style={{ height: "10rem" }}
-                                        data-sb-validations="required"
-                                        data-sb-can-submit="no"
-                                        defaultValue={""}
-                                    />
-                                    <label htmlFor="message">Message</label>
-                                    <div className="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
-                                </div>
-                                {/* Submit success message*/}
-                                {/**/}
-                                {/* This is what your users will see when the form*/}
-                                {/* has successfully submitted*/}
-                                <div className="d-none" id="submitSuccessMessage">
-                                    <div className="text-center mb-3">
-                                        <div className="fw-bolder">Form submission successful!</div>
-                                        To activate this form, sign up at
-                                        <br />
-                                        <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms
-                                        </a>
+                </header>
+                {/* Begin Contact */}
+                <section className="page-section" id="contact">
+                    <div className="container">
+                        {/* Contact Section Heading*/}
+                        <h2 className="page-section-heading text-center text-uppercase text-secondary mb-3">
+                            Liên hệ với chúng tôi
+                        </h2>
+                        {/* Contact Section Form*/}
+                        <div className="row justify-content-center">
+                            <div className="col-lg-8 col-xl-7">
+                                <form name="sentMessage" id="contactForm" noValidate="novalidate">
+                                    <div className="control-group">
+                                        <div className="form-group floating-label-form-group controls mb-0 pb-2">
+                                            <label>Name</label>
+                                            <input type="text" id="name" className="form-control" placeholder="Name" required=""/>
+                                            <p className="help-block text-danger" />
+                                        </div>
                                     </div>
-                                </div>
-                                {/* Submit error message*/}
-                                {/**/}
-                                {/* This is what your users will see when there is*/}
-                                {/* an error submitting the form*/}
-                                <div className="d-none" id="submitErrorMessage">
-                                    <div className="text-center text-danger mb-3">Error sending message!</div>
-                                </div>
-                                {/* Submit Button*/}
-                                <button className="btn btn-primary btn-xl disabled" id="submitButton" type="submit" >Send</button>
-                            </form>
+                                    <div className="control-group">
+                                        <div className="form-group floating-label-form-group controls mb-0 pb-2">
+                                            <label>Email Address</label>
+                                            <input type="email" id="email" className="form-control" placeholder="Email Adress" required=""/>
+                                            <p className="help-block text-danger" />
+                                        </div>
+                                    </div>
+                                    <div className="control-group" />
+                                    <div className="form-group floating-label-form-group controls mb-0 pb-2">
+                                        <label>Phone Number</label>
+                                        <input type="tel" id="phone" className="form-control" placeholder="Phone Number" required="" />
+                                        <p className="help-block text-danger" />
+                                    </div>
+                                    <div className="control-group" />
+                                    <div className="form-group floating-label-form-group controls mb-0 pb-2">
+                                        <label>Message</label>
+                                        <textarea className="form-control" id="message" rows="{5}" placeholder="message" required="" defaultValue={""} />
+                                        <p className="help-block text-danger" />
+                                    </div>
+                                    <br />
+                                    <div className="form-group">
+                                        <button type="submit" onClick={(event) => this.submitForm(event)} className="btn btn-primary" id="sendMessageButton">Send</button>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
-            </>             
+                </section>
+            </>
         );
     }
 }
